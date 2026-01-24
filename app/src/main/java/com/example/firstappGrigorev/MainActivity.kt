a1207d54bdeaefca778ae668fb1fc0dd1f195bc8
SilverV-silent <koss.koss.koss07@gmail.com>
2026-01-24 16:10:46 +0300

    One more time

--- app/src/main/java/com/example/firstappGrigorev/MainActivity.kt
+++ app/src/main/java/com/example/firstappGrigorev/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.firstappGrigorev
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.firstappGrigorev.R
+import kotlin.random.Random
+class MainActivity : Activity() {
+    private lateinit var rollButton: Button
+    private lateinit var resultTextView: TextView
+    private lateinit var diceNumberTextView: TextView
 
-class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Привязываем компоненты
+        rollButton = findViewById(R.id.button)
+        resultTextView = findViewById(R.id.textView2)
+        diceNumberTextView = findViewById(R.id.textViewResult)
+
+        // Обрабатываем клик по кнопке
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        val diceValue = Random.nextInt(1, 7) // Генерация случайного числа от 1 до 6
+
+        // Отображение результата в первом тексте
+        resultTextView.text = "Выпал номер: $diceValue"
+
+        // Вывод самой цифры крупным шрифтом
+        diceNumberTextView.text = "$diceValue"
     }
 }
